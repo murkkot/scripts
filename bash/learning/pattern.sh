90c4123551854b8e7bf17f5831a77f8d581b0926
Anton Kuvaev <kuvaev.anton@gmail.com>
2025-10-29 16:40:44 +0300

    Add pattern.sh script

--- bash/learning/pattern.sh
+++ bash/learning/pattern.sh
@@ -3,18 +3,16 @@
 echo "[INPUT]Enter  the value of N[INPUT]"
 read user_input
 
-line_count=1
 counter=1
 
 if [[ "$user_input" =~ ^[0-9]+$ ]]; then
-    for (( i=0; i<$user_input; i++)); do
-        for (( j=0; j<$line_count; j++ )); do
+    for (( i=1; i<=$user_input; i++)); do
+        for (( j=1; j<$i; j++ )); do
             echo -n "$counter"
             echo -n ' '
             (( counter++ ))
         done
         echo ''
-        (( line_count++ ))
     done
 else
     echo "You should enter a number"
