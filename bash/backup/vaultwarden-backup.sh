0a9b9e84a055653f3a58588a9fd00be0d7edca53
murkkot <kuvaev.anton@gmail.com>
2025-11-13 13:55:48 +0300

    Modified vaultwarden-backup script

--- bash/backup/vaultwarden-backup.sh
+++ bash/backup/vaultwarden-backup.sh
@@ -1,4 +1,4 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 
 # current date
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
@@ -8,22 +8,22 @@ backup_path="$1"
 printf '%s Backup job started\n' "$current_date"
 
 if [ $# -ne 1 ]; then
-    printf 'Usage: %s [BACKUP PATH]\n' "${0##*/}" >&2
+    printf 'Wrong parameters. Usage: %s [BACKUP PATH]\n' "${0##*/}" >&2
     exit 2
 fi
 
 if [ ! -d "$backup_path" ]; then
-    printf "Backup dir doesn't exists" >&2
+    printf "ERROR Backup dir doesn't exists\n" >&2
     exit 2
 fi
 
-docker run --rm --volumes-from=vaultwarden -e UID=0 -e BACKUP_DIR="$backup_path" -e TIMESTAMP=true -v "$backup_path":/myBackup bruceforce/vaultwarden-backup manual
+docker run --rm --volumes-from=vaultwarden -e UID=1000 -e BACKUP_DIR=/myBackup -e TIMESTAMP=true -v "$backup_path":/myBackup bruceforce/vaultwarden-backup manual
 
 # check exit status
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
-    printf 'Backup failed' >&2
+    printf 'ERROR Backup failed\n' >&2
     exit $exit_code
 else
-    printf 'Backup successful'
+    printf 'Backup successful\n'
 fi
