a256f2577482fc188570f6b6390a132cd6e6cd2a
murkkot <kuvaev.anton@gmail.com>
2025-11-13 14:16:42 +0300

    Modify vaultwarden backup script

--- bash/backup/vaultwarden-backup.sh
+++ bash/backup/vaultwarden-backup.sh
@@ -19,7 +19,7 @@ if [ ! -d "$backup_path" ]; then
     exit 2
 fi
 
-docker run --rm --volumes-from=vaultwarden -e UID=1000 -e BACKUP_DIR=/myBackup -e TIMESTAMP=true -v "$backup_path":/myBackup bruceforce/vaultwarden-backup manual
+docker run --rm --volumes-from=vaultwarden --privileged -e UID=1 -e BACKUP_DIR=/myBackup -e TIMESTAMP=true -v "$backup_path":/myBackup bruceforce/vaultwarden-backup manual
 
 # check exit status
 exit_code=$?
@@ -34,7 +34,7 @@ fi
 current_archive_num=$(ls "$backup_path"/*backup.tar.xz 2>/dev/null | wc -l)
 
 if [ $current_archive_num -ge $archives_num ]; then
-    oldest_archive=$(ls $backup_dir/*backup.tar.xz | head -n 1)
+    oldest_archive=$(ls $backup_path/*backup.tar.xz | head -n 1)
     rm $oldest_archive
     echo "Removed file $oldest_archive"
 fi
