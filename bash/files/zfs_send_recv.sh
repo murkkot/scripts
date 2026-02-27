b2eb9cc682f65d90b6da4d939a84846621040350
Anton Kuvaev <kuvaev.anton@gmail.com>
2026-02-27 10:45:30 +0300

    Modify zfs_send_recv script

--- bash/files/zfs_send_recv.sh
+++ bash/files/zfs_send_recv.sh
@@ -4,25 +4,26 @@
 
 dataset="$1"
 
-printf 'Create snapshot'
+printf 'Create snapshot\n'
 zfs snapshot -r storage/"$dataset"@rewrite1
 
-stream_size=$(zfs send -n -v -R --raw storage/photos@rewrite1 | grep -oE '[0-9]+G' | tail -n1)
-printf 'Estimated stream size: %s' "$stream_size"
+stream_size=$(zfs send -n -v -R --raw storage/"$dataset"@rewrite1 | grep -oE '[0-9.]+[GK]' | tail -n1)
+printf 'Estimated stream size: %s\n' "$stream_size"
 zfs send -R --raw storage/"$dataset"@rewrite1 | pv -s "$stream_size" | zfs recv -u storage/"$dataset"_new
 
-printf 'Copied'
+printf 'Copied\n'
 zfs list -r storage/"$dataset" storage/"$dataset"_new
 
-printf 'Swap dataset names'
+printf 'Swap dataset names\n'
 zfs unmount -f storage/"$dataset"
 zfs rename storage/"$dataset" storage/"$dataset"_old
 zfs rename storage/"$dataset"_new storage/"$dataset"
 zfs set mountpoint=/storage/"$dataset" storage/"$dataset"
 zfs set canmount=on storage/"$dataset"
-printf 'Load dataset key and change key to root'
+printf 'Load dataset key and change key to root\n'
 zfs load-key storage/"$dataset"
 zfs change-key -i storage/m"$dataset"
 zfs get encryptionroot storage/"$dataset"
-printf 'Mount dataset'
+printf 'Mount dataset\n'
 zfs mount storage/"$dataset"
+zfs list -r storage/"$dataset" storage/"$dataset"_old
