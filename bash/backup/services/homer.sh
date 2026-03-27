e17addccf9683712b618ef33bccd8028836f09b8
Anton Kuvaev <kuvaev.anton@gmail.com>
2026-03-27 17:23:08 +0300

    Add homer backup script

---
+++ bash/backup/services/homer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TMP_DIR=$1
+SOURCE_DIR=$(docker inspect homer | \
+ jq -r '.[].Config.Labels["com.docker.compose.project.working_dir"]')
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+tar -czf ${TMP_DIR}/$TIMESTAMP.tar.gz "$SOURCE_DIR" >/dev/null 2>&1
+
+echo "$TMP_DIR/$TIMESTAMP.zip"
